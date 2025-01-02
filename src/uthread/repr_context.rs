86e86c6648117b362457810708a0d014e03570d5
Tomás Vallotton <tomi@ruuf.solar>
2025-01-02 00:29:16 +0000

    ignore mutex tests since mutex is currently deadlocking

--- src/uthread/repr_context.rs
+++ src/uthread/repr_context.rs
@@ -4,9 +4,9 @@ use std::{
     cell::UnsafeCell,
     io,
     mem::zeroed,
-    ptr::{null_mut, NonNull},
+    ptr::NonNull,
     sync::{
-        atomic::{AtomicBool, AtomicI64, AtomicPtr, AtomicU64, AtomicU8, Ordering},
+        atomic::{AtomicBool, AtomicI64, AtomicU64, AtomicU8, Ordering},
         Mutex,
     },
 };
